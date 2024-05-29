7e96234518c1bce2740d9b1acb08498172243f87
mirrorganger <mirrorganger@users.noreply.github.com>
2024-05-29 22:02:31 +0000

    deploy: f7727edb9a1febdd174b5310a7bc65340dae8700

---
+++ search/groups_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['old_20gpio_0',['DEPRECATED-OLD-GPIO',['../group___d_e_p_r_e_c_a_t_e_d-_o_l_d-_g_p_i_o.html',1,'']]]
+];
---
+++ structdaisy_1_1_abstract_menu_1_1_item_config.js
@@ -0,0 +1,16 @@
+var structdaisy_1_1_abstract_menu_1_1_item_config =
+[
+    [ "asCallbackFunctionItem", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a1672d00d3071598761c92964e9dedc74", null ],
+    [ "asCheckboxItem", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a14cccd67ecc352adcb0195726831377d", null ],
+    [ "asCustomItem", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a106a6619056f6d32fd9d0fab8ef8ed4f", null ],
+    [ "asMappedValueItem", "structdaisy_1_1_abstract_menu_1_1_item_config.html#af65b22696eecd313c8b6cdafb012a2fc", null ],
+    [ "asOpenUiPageItem", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a65ed0caa968a72c234efe79602a42cb4", null ],
+    [ "callbackFunction", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a2b6c4878bca2058984ab5b3ff163fe08", null ],
+    [ "context", "structdaisy_1_1_abstract_menu_1_1_item_config.html#ae7c2d4b97bd035919b52d46bf9cb659e", null ],
+    [ "itemObject", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a99f29482e671fe3d1193123dc1f4bdc9", null ],
+    [ "pageToOpen", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a0fe2cb5f1b07d1245121bee055e4a0d1", null ],
+    [ "text", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a7d9c5d9a01014d1369c21e5d175b1f6a", null ],
+    [ "type", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a0b79dcaa186542997e04669b92d821ae", null ],
+    [ "valueToModify", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a486b6e91fe2d329846a3dd81572ceeb5", null ],
+    [ "valueToModify", "structdaisy_1_1_abstract_menu_1_1_item_config.html#a90d6beb8e952dd0526a63c74c2d5f22d", null ]
+];
